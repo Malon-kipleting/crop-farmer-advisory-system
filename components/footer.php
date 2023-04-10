8e77fe7b91ac08f6c0c7123fcba59d627e75a129
makaubenson <bensonmakau2000@gmail.com>
2023-04-10 19:24:24 +0300

    Redesigned login page

--- components/footer.php
+++ components/footer.php
@@ -1,11 +1,11 @@
 <!-- Footer -->
-<footer class="page-footer font-small blue fixed-bottom">
+<footer class="page-footer font-small blue fixed-bottom w3-green">
 
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2023 Copyright:
-    <a href="/"> Crop Farmer Advisory System</a>
-  </div>
-  <!-- Copyright -->
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3" style="font-size:23px; padding:15px">© 2023 Copyright:
+        <a> Crop Farmer Advisory System</a>
+    </div>
+    <!-- Copyright -->
 
 </footer>
 <!-- Footer -->
